ccbf5224df230b7bd65b8f5be0c3dee3fb6a18e2
DmytroVoitovych <begunec23@gmail.com>
2022-07-21 13:18:03 +0200

    ready

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import user from './jsonData/user.json';
+import data from './jsonData/data.json';
+import friends from "./jsonData/friends.json";
+import transactions from "./jsonData/transactions.json";
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Stats/Stats';
+import FriendList from './components/FriendsList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        
+    paddingTop: '20px',
+        }}
+    >
+     <Profile
+    username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+      
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
+//test
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'App';
 import './index.css';
+import React from 'react';
 
+//новый синтаксис 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    </React.StrictMode>
 );
+
+
+
+
+
+// ReactDOM.render(painting, document.querySelector('#root'));
